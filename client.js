462247f2ca4bfc6d039e4c8cedf82f70bdf58d8d
mellaniesoriano <msoriano808@gmail.com>
2017-06-30 22:54:41 -1000

    client now connecting and writing?

--- client.js
+++ client.js
@@ -3,7 +3,16 @@ const net = require('net');
 
 const server = require('./server.js');
 
-// connect client to server
-const client = net.connect({port: 6969, host: '0.0.0.0'}, () => {
-  console.log('You are now connected');
+var client = new net.Socket();
+client.connect({port: 6969, host: '0.0.0.0'}, () => {
+  console.log('new connection');
+  client.write('Hello server!');
+});
+
+client.on('data', (data) => {
+  console.log(`Received:  ${data}`);
+});
+
+client.on('close', () => {
+  console.log('Connection closed');
 });
