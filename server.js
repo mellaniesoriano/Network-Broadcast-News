32af868bf0641e16ff2a0204074425a77e2bde1b
mellaniesoriano <msoriano808@gmail.com>
2017-07-01 15:33:05 -1000

    servers relaying msg to all clients, working on eliminating duplicate writes

--- server.js
+++ server.js
@@ -2,14 +2,37 @@
 
 const net = require('net');
 
+const clients = [];
+
 const server = net.createServer((connection) => {
-  connection.write('Echo server\n');
   connection.pipe(connection);
-  connection.on('data', (data) => {
-});
+  connection.on('data', (chunk) => {
+    console.log(`Server From Client: ${chunk}`);
+
+  // Identify the client
+  connection.name = connection.remoteAddress + ":" + connection.remotePort;
+  });
+
+  clients.push(connection);
+
+  connection.on('data', (chunk, sender) => {
+    clients.forEach( (client) => {
+      if (client === sender) {
+        return;
+      } else {
+        client.write(chunk);
+      }
+    });
+  });
+
 });
 
+
 // listening to client
-server.listen({port: 6969, host: '0.0.0.0'}, () => {
+server.listen(6969, '0.0.0.0', () => {
   console.log('Server listening on port 6969');
 });
+
+//based on events server should relay to multiple clients
+
+// 59894
