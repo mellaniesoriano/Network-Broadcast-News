bfc0fcd7061f25b544e023ae8e85b477bb0a298a
mellaniesoriano <msoriano808@gmail.com>
2017-07-01 15:59:01 -1000

    removed connection.pipe, removed extra write

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const net = require('net');
 const clients = [];
 
 const server = net.createServer((connection) => {
-  connection.pipe(connection);
   connection.on('data', (chunk) => {
     console.log(`Server From Client: ${chunk}`);
 
@@ -32,7 +31,3 @@ const server = net.createServer((connection) => {
 server.listen(6969, '0.0.0.0', () => {
   console.log('Server listening on port 6969');
 });
-
-//based on events server should relay to multiple clients
-
-// 59894
