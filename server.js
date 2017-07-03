fe242874be01f6ee2b00a59418e0b921e211b5db
mellaniesoriano <msoriano808@gmail.com>
2017-07-02 20:34:17 -1000

    username registration works and added menu

--- server.js
+++ server.js
@@ -3,14 +3,36 @@
 const net = require('net');
 
 const clients = [];
+const changeUsername = [];
+const userMenu = `
+  @ : Change Username : '@sample'
+`;
+
 
 const server = net.createServer((connection) => {
+  connection.userName = `User# ${clients.length}`;
+
+  // var testing = changeUsername.push(connection.userName);
+  // console.log('changeUserArr..', testing);
+
+  connection.write(`*** Welcome  *** \n Get started with menu : \n ${userMenu}`);
+
   connection.on('data', (chunk) => {
+    data = changeUsername.slice(1, -1);
     console.log(`Server From Client: ${chunk}`);
 
-  // Identify the client
-  connection.name = connection.remoteAddress + ":" + connection.remotePort;
-  });
+    switch(chunk.toString().charAt(0)) {
+      case '@':
+      clients.forEach( (element) => {
+        if (element === connection) {
+          changeUsername.push(`${connection.userName}: ${chunk.toString()}`);
+          connection.userName = chunk.toString().slice(1, chunk.length -1);
+        }
+      });
+      } // end switch statement
+    });
+
+
 
   clients.push(connection);
 
@@ -19,13 +41,13 @@ const server = net.createServer((connection) => {
       if (client === sender) {
         return;
       } else {
-        client.write(chunk);
+        client.write(`${connection.userName} : ${chunk}`);
       }
     });
   });
 
-});
-
+  console.log(clients.length);
+}); // createServer ends
 
 // listening to client
 server.listen(6969, '0.0.0.0', () => {
