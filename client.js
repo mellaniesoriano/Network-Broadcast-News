296be2cde31e8e37dc1e2ee8c58f7a76ae76934b
mellaniesoriano <msoriano808@gmail.com>
2017-07-04 18:09:44 -1000

    removed console log

--- client.js
+++ client.js
@@ -2,7 +2,6 @@
 const net = require('net');
 
 const client = net.connect(6969, '0.0.0.0', () => {
-  console.log('new connection');
 
   // writes msg to server
   process.stdin.on('data', (chunk) => {
