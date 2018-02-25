89b8e009e23d174e3aa0b6f527998992eb7922e4
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2018-02-24 16:01:23 -1000

    features

--- client.js
+++ client.js
@@ -1,15 +1,17 @@
-/*jshint esversion: 6*/
 const net = require('net');
 
-const client = net.connect(6969, '0.0.0.0', () => {
-
+const client = net.connect(3000, '0.0.0.0', () => {
   // writes msg to server
-  process.stdin.on('data', (chunk) => {
+  process.stdin.on('data', chunk => {
     client.write(chunk);
   });
 
   // receives msg from server
-  client.on('data', (chunk) => {
+  client.on('data', chunk => {
     console.log(chunk.toString());
   });
+});
+
+client.on('error', err => {
+  console.log(err);
 });
--- server.js
+++ server.js
@@ -1,69 +1,189 @@
-/* jshint esversion: 6 */
-
 const net = require('net');
+//Data Arrays
+const clientArr = [];
+const fullChatLog = [];
+const nameChangeLog = [];
+const privateMsgLog = [];
+const serverMsgLog = [];
 
-const clients = [];
-const changeUsername = [];
-
-const userMenu = `[ USER MENU ]
-  @ : Change Username : '@sampleUsername'
+//User Tools Key
+const specialKeys = `
+  ? : Help
+  $ : Set Username:  '@sample'
+  / : Private Message to Admin : '/msg'
+  @ : Private Message: '@User + Msg'
+  ~ : See User List
 `;
-
-const adminMenu = `[ ADMIN MENU ]
-  ! : Kick User : '!Username'
+//Admin Tools Key
+const adminTools = `
+  ? : Help
+  ! : Kick User: '!sampleUser'
+  @ : Private Message: '@User Msg'
+  ~ : See User List
+  * : Read Private Message Log
+  $ : Read Name Change Log
+  % : Read Chat Log
+  # : Read Direct Server Message Log
 `;
+//Custom Message Divider
+const divider = '-----------------';
 
-
-const server = net.createServer((connection) => {
+const server = net.createServer(connection => {
+  //Server Admin
   server.userName = '[ADMIN]';
-  connection.userName = `User #${clients.length}`;
-  clients.push(connection);
+  //When socket first connects, give anonymous userName/send user Tools
+  clientArr.push(connection);
+  connection.userName = `Anonymous${clientArr.length}`;
+  connection.msgTimer = [];
 
-  connection.write(`*** Welcome To The Chatroom *** \n ${userMenu}`);
+  connection.write(`${specialKeys}\nENJOY CHATTING!\n${divider}`);
+  //Event Listener for when data comes in. Checks for character for special tools
+  connection.on('data', data => {
+    switch (data.toString().charAt(0)) {
+      case '?': //Help - Send User Tools
+        connection.write(`${specialKeys}`);
+        break;
 
-  connection.on('data', (chunk) => {
-    console.log(`Msg from ${connection.userName}: ${chunk}`);
-
-    switch(chunk.toString().charAt(0)) {
-      case '@':
-      clients.forEach( (element) => {
-        if (element.userName === chunk.toString().slice(1, chunk.length -1).indexOf('admin') !== -1) {
-          element.write('Changing username to ADMIN is not permitted!!');
+      case '$': //User request to change name
+        if (clientArr.some((element) => element.userName === data.toString().slice(1, -1) || data.toString().slice(1, -1).toLowerCase().indexOf('admin') > 0)) {
+          connection.write('Username Invalid or In Use!');
         } else {
-          changeUsername.push(`${connection.userName}: ${chunk.toString()}`);
-          connection.userName = chunk.toString().slice(1, chunk.length -1);
+          clientArr.forEach((element) => {
+            if (element === connection) {
+              nameChangeLog.push(`${connection.userName}: ${data.toString()}`);
+              connection.userName = data.toString().slice(1, -1);
+            }
+          });
+        }
+        break;
+
+      case '/': //User sends private message to admin
+        console.log(
+          `(${connection.userName}) (${data.toString().slice(1, -1)})`
+        );
+        serverMsgLog.push(`${connection.userName}: ${data.toString()}`);
+        break;
+
+      case '@': //Private Message
+        let privateUser = data
+          .toString()
+          .slice(1, data.toString().indexOf(' '));
+        let message = data
+          .toString()
+          .slice(data.toString().indexOf(' '))
+          .slice(1);
+        clientArr.forEach(element => {
+          if (element.userName === privateUser) {
+            element.write(
+              `**Direct Message from ${connection.userName}: ${message}`
+            );
+          }
+        });
+        privateMsgLog.push(`${connection.userName}: ${data.toString()}`);
+        break;
+
+      case '~': //Request to see user list
+        connection.write(
+          `${clientArr.map(element => element.userName).join(', ')}`
+        );
+        break;
+
+      default:
+        //Default user sends message to group
+        clientArr.forEach(element => {
+          if (element !== connection) {
+            element.write(
+              `${connection.userName}: ${data
+                .toString()
+                .slice(0, -1)}\n${divider}`
+            );
+            fullChatLog.push(`${connection.userName}: ${data.toString()}`);
+          } else {
+            element.write(`${divider}`);
+          }
+        });
+    }
+    //spam filter start
+    connection.msgTimer.push({
+      time: new Date().getTime() //Creates object that records time of message
+    });
+    //make sure at least 5 messages have been sent
+    if (connection.msgTimer.length >= 8) {
+      //check if last message came within 5 seconds of the 5th message from end
+      if (
+        connection.msgTimer[connection.msgTimer.length - 1].time -
+        connection.msgTimer[connection.msgTimer.length - 8].time <=
+        8000
+      ) {
+        connection.end('GTFO! KICKED FOR SPAMMING! >;(');
+      }
+    }
+  });
+});
+
+server.listen({
+    port: 3000
+  },
+  () => {
+    console.log(`Connection is ON\n${adminTools}`);
+  }
+);
+
+//Server Admin chats and admin tools
+process.stdin.on('data', data => {
+  switch (data.toString().charAt(0)) {
+    case '?': //Display admin tools
+      // console.log(adminTools);
+      break;
+
+    case '!': //Kick User
+      clientArr
+        .filter(element => {
+          return element.userName === data.toString().slice(1, -1);
+        })
+        .forEach(element => {
+          clientArr.splice(clientArr.indexOf(element, 1));
+          element.end(`${server.userName}: You Have Been Kicked`);
+        });
+      break;
+
+    case '@': //Send private Message to user
+      let privateUser = data.toString().slice(1, data.toString().indexOf(' '));
+      let message = data
+        .toString()
+        .slice(data.toString().indexOf(' '))
+        .slice(1);
+      clientArr.forEach(element => {
+        if (element.userName === privateUser) {
+          element.write(`***${server.userName} - PRIVATE - : ${message}`);
         }
       });
       break;
-      default:
-      clients.forEach( (element) => {
-        element.write(`${connection.userName}: ${chunk}`);
-        });
-      }// end switch statement
-    });
 
-}); // createServer ends
+    case '~': //Request to see user list
+      console.log(`${clientArr.map(element => element.userName).join(', ')}`);
+      break;
 
-process.stdin.on('data', (chunk) => {
-  switch (chunk.toString().charAt(0)) {
-    case '!' :
-    clients.filter( (element) => {
-      return element.userName === chunk.toString().slice(1, chunk.length -1);
-    })
-    .forEach( (element) => {
-      clients.splice(clients.indexOf(element, 1));
-      element.end(`${server.userName}: You have been kicked!`);
-    });
-    break;
+    case '*': //Read Private Message Log
+      console.log(privateMsgLog.join(''));
+      break;
+
+    case '$': //Read name change log
+      console.log(nameChangeLog.join(''));
+      break;
+
+    case '%': //read chat log
+      console.log(fullChatLog.join(''));
+      break;
+
+    case '#': // read user to admin message log
+      console.log(serverMsgLog.join(''));
+      break;
 
     default:
-    clients.forEach( (client) => {
-      client.write(`${server.userName}: ${chunk}`);
-    });
-  } // end switch statement
-});
-
-// listening to client
-server.listen(6969, '0.0.0.0', () => {
-  console.log(`Server listening on port 6969 \n ${adminMenu}`);
+      //Broadcast Message to all
+      clientArr.forEach(element => {
+        element.write(`${server.userName} : ${data}`);
+      });
+  }
 });
